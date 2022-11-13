3e99ca421b96625d40362cc896967c72d9c83c2e
Eric Sans <esansvi@gmail.com>
2022-11-14 00:17:00 +0100

    Exercise 16 non-argument constructor

---
+++ src/Segment.java
@@ -0,0 +1,13 @@
+public class Segment {
+    //2 Point fields set on private
+    private Point startPoint;
+    private Point endPoint;
+
+    public Segment(){
+        startPoint.setX(0);
+        startPoint.setY(0);
+        endPoint.setX(0);
+        endPoint.setY(0);
+    }
+
+}
