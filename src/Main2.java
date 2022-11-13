71c8a72350805f6bb32a1d832c666b35168ce2f9
Eric Sans <esansvi@gmail.com>
2022-11-13 23:58:17 +0100

    Exercise 14

--- src/Main2.java
+++ src/Main2.java
@@ -2,5 +2,11 @@ public class Main2 {
     public static void main(String[] argv){
         Point p1 = new Point (4,5);
         Point p2 = new Point (6,8);
+
+        p1.setOffset(4,4);
+        p2.setOffset(4,4);
+        //testing that setOffset works
+        //System.out.println(p2);
+
     }
 }
