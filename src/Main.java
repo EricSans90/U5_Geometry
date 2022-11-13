1fa80529b319298074855fdb72f34d9908941433
Eric Sans <esansvi@gmail.com>
2022-11-13 22:38:49 +0100

    Exercise 5 and Main with setX setY

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] argv){
+        Point p = new Point(3,4);
+        p.setX(12);
+    }
+}
--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,34 @@
 public class Point {
+    //2 int fields
+    private int x = 0;
+    private int y = 0;
+
+    //a no parameter constructor
+    public Point (){
+        x=0;
+        y=0;
+    }
+
+    //a 2 type int parameter constructor
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    //public method to set non negative X
+    //public void setX(int newX){
+    //    if newX (<0){
+    //        x=-newX;
+    //    } else {
+    //        x=newX;
+    //    }
+    //}
+
+    public void setX(int newX){
+        x=newX;
+    }
+
+    public void setY(int newY){
+        y=newY;
+    }
 }
