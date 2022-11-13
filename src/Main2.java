b393e139b0588b92cbfaf827483fbfe6e71bbc7f
Eric Sans <esansvi@gmail.com>
2022-11-14 00:52:15 +0100

    Eric Sans Vidal - Exercise 18 module method + tested

--- src/Main2.java
+++ src/Main2.java
@@ -1,10 +1,11 @@
 public class Main2 {
     public static void main(String[] argv){
+
         Point p1 = new Point (4,5);
         Point p2 = new Point (6,8);
 
-        p1.setOffset(4,4);
-        p2.setOffset(4,4);
+        //p1.setOffset(4,4);
+        //p2.setOffset(4,4);
         //printing both points
         System.out.println(p1);
         System.out.println(p2);
@@ -12,5 +13,8 @@ public class Main2 {
         //System.out.println("Point 1 is= "+p1.toString());
         //System.out.println("Point 2 is= "+p2.toString());
 
+        //Testing module method
+        //Segment stest = new Segment (p1,p2);
+        //System.out.println(stest.module(stest));
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Segment {
     //2 Point fields set on private
     private Point startPoint;
@@ -15,5 +17,25 @@ public class Segment {
     public Segment(Point p1, Point p2){
         startPoint=p1;
         endPoint=p2;
+    }
+
+    public Point getStartPoint(){
+        return startPoint;
+    }
+    public Point getEndPoint(){
+        return endPoint;
+    }
+
+    //method module
+    public double module(Segment s1){
+        double x1=0, x2=0, y1=0, y2=0, xDistance=0, yDistance=0;
+        x1=s1.getStartPoint().getX();
+        y1=s1.getStartPoint().getY();
+        x2=s1.getEndPoint().getX();
+        y2=s1.getEndPoint().getY();
+        xDistance = x2-x1;
+        yDistance = y2-y1;
+
+        return Math.sqrt(Math.pow(xDistance,2)+Math.pow(yDistance,2));
     }
 }
