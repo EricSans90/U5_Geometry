708f588532ffa1d9bb16c7d921e3be241cfbfab6
Eric Sans <esansvi@gmail.com>
2022-11-14 00:01:17 +0100

    Exercise 15

--- src/Main2.java
+++ src/Main2.java
@@ -5,8 +5,12 @@ public class Main2 {
 
         p1.setOffset(4,4);
         p2.setOffset(4,4);
-        //testing that setOffset works
-        //System.out.println(p2);
+        //printing both points
+        System.out.println(p1);
+        System.out.println(p2);
+        //printing both points with to string
+        //System.out.println("Point 1 is= "+p1.toString());
+        //System.out.println("Point 2 is= "+p2.toString());
 
     }
 }
