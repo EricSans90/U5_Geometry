c638e9fda4005d2175f0ca7609cc17790705b72c
Eric Sans <esansvi@gmail.com>
2022-11-14 18:47:58 +0100

    Eric Sans Vidal - Exercise 30 create Rectangle + Main

---
+++ src/Main4.java
@@ -0,0 +1,8 @@
+public class Main4 {
+    public static void main (String[] argv){
+        Point p = new Point(2, 3);
+        Rectangle r = new Rectangle(p, 4, 2);
+        System.out.println(p);
+        //System.out.println(r);
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    private Point p;
+    private int width=0;
+    private int height=0;
+
+    public Rectangle(Point p, int width, int height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+}
