7c5d0e4e13f68f68cfa5d8b720bd0a0538a8efbd
Eric Sans <esansvi@gmail.com>
2022-11-14 19:44:49 +0100

    Eric Sans Vidal - Exercise 3 Main4 rect + print + move + print

--- src/Main4.java
+++ src/Main4.java
@@ -2,8 +2,16 @@ public class Main4 {
     public static void main (String[] argv){
         Point p = new Point(2, 3);
         Rectangle r = new Rectangle(p, 4, 2);
-        System.out.println(p);
-        //System.out.println(r);
-        Rectangle R = new Rectangle();
+        System.out.println(r.toString());
+        p.setX(4);
+        p.setY(7);
+        System.out.println(r.toString());
+        // System.out.println(p);
+
+        //Point t = new Point (r.getPoint().getX(),r.getPoint().getY());
+        //System.out.println(t);
+        //System.out.println(r.toString());
+        //Rectangle R = new Rectangle();
+        //System.out.println(r.toString());
     }
 }
