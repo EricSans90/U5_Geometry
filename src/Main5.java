942557222ffcc3daa8ecf86abce2894e36e00d71
Eric Sans <esansvi@gmail.com>
2022-11-15 01:26:11 +0100

    Eric Sans Vidal - Exercise 49 initialize the array to the points

--- src/Main5.java
+++ src/Main5.java
@@ -1,6 +1,11 @@
 public class Main5 {
     public static void main (String[] argv){
         Point[] points;
+        points = new Point[10];
 
+        for (int i = 0; (i < 10); i++) {
+            points[i]=new Point(i,i);
+
+        }
     }
 }
