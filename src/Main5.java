eea1987c0b60054c56ece50af5b39d679ac80084
Eric Sans <esansvi@gmail.com>
2022-11-15 01:31:25 +0100

    Eric Sans Vidal - Exercise 51

--- src/Main5.java
+++ src/Main5.java
@@ -7,5 +7,11 @@ public class Main5 {
             points[i]=new Point(i,i);
             System.out.println(points[i]);
         }
+        System.out.println();
+        Rectangle[] rectangles = new Rectangle[10];
+        for (int i = 0; (i < 10); i++) {
+            rectangles[i]=new Rectangle(points[i],1,1);
+            System.out.println(rectangles[i]);
+        }
     }
 }
